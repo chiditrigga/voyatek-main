f0702648ecfa51886c8650ca9261d14b01016a06
daniel <chididaniel1998@gmail.com>
2024-12-16 06:06:29 -0800

    reduced font

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -13,9 +13,9 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description, buttonLabel, onButtonClick, bgColor, textColor,btcolor,btext }) => {
   return (
-    <div className={`px-3 py-4 rounded-md ${bgColor} ${textColor} md:w-fit w-full lg:w-72`}>
+    <div className={`px-3 py-4 rounded-md ${bgColor} ${textColor} md:w-fit w-full lg:w-64`}>
       <h3 className="text-base font-semibold mb-2">{title}</h3>
-      <p className="mb-4 font-normal text-sm">{description}</p>
+      <p className="mb-4 font-normal  text-xs">{description}</p>
       <button
         onClick={onButtonClick}
         className={`mt-5 w-full text-sm px-4 py-2 ${btcolor} ${btext} font-normal rounded hover:opacity-80`}
